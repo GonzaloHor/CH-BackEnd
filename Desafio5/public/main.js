7cc958bf3c2e5d2fe7200c8a89ba6f74bfac6ed4
cls <gonzalo_horton@outlook.com>
2022-03-27 16:04:29 -0300

    Desafio5

--- Desafio5/public/main.js
+++ Desafio5/public/main.js
@@ -1,26 +1,80 @@
 const socket = io.connect();
-socket.on('messages', data => {
-    console.log(data);
-});
+
 
 
 function render(data) {
+
     const html = data.map((elem, index) => {
-        return(`<div>
-            <strong>${elem.author}</strong>:
-            <em>${elem.text}</em> </div>`)
+        return(`<div class="card m-4 productos" style="width: 18rem;">
+            <img src=${elem.Thumbnail} class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Titulo ${elem.Title}</h5>
+            <h5 class="card-title">Precio $ ${elem.Price}</h5>
+            <h5 class="card-title">Id ${elem.id}</h5>
+            <button class="btn btn-warning m-1" onclick="deleteProduct(${elem.id})"" >Eliminar </button>
+            </div></div>`)
     }).join(" ");
-    document.getElementById('messages').innerHTML = html;
+    console.log("Renderizando productos")
+
+    document.getElementById('productos').innerHTML = html;
 }
 
-socket.on('messages', function(data) { render(data); });
+
+function renderMensajes(data) {
+
+    const mensaje = data
+    if(mensaje == "No hay contenido") {
+
+        const html = "<h3>No hay mensajes registrados<h3>"
+    
+        document.getElementById('menssages').innerHTML = html;
+
+    } else {
+        const html = mensaje.map((elem, index) => {
+            return(`<div>
+               <p class="m-2">[${elem.Email}</p>
+               <p class="m-2">${elem.dataTimeMensaje}]</p>
+               <p class="m-2 mensaje">: ${elem.Mensaje}</p>
+               </div>`)
+        }).join(" ");
+    
+        document.getElementById('menssages').innerHTML = html;
+    }
+
+}
+
+
+socket.on('productos', function(data) { render(data); });
+socket.on('mensajes', function(data) { renderMensajes(data); });
+
+function addProducto(e) {
+    const producto = {
+        Title: document.getElementById('Title').value,
+        Price: document.getElementById('Price').value,
+        Thumbnail: document.getElementById('Thumbnail').value
+    };
+    console.log("Esta cargando productos")
+
+    socket.emit('new-producto', producto);
+    return false;
+}
+
 
 function addMessage(e) {
     const mensaje = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
+        Email: document.getElementById('Email').value,
+        Mensaje: document.getElementById('Mensaje').value,
     };
-    socket.emit('new-message', mensaje);
+  
+    socket.emit('new-mensaje', mensaje);
     return false;
 }
+
+
+function deleteProduct(id) {
+
+    socket.emit('delete-product', id);
+    return false;
+}
+
 
--- Desafio5/server.js
+++ Desafio5/server.js
@@ -1,33 +1,191 @@
-const express = require('express');
+const express = require('express')
+const { Router } = express
+const app = express()
+const router = Router()
+const fs = require('fs')
+const alert = require('alert'); 
+
+
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
 
-const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+class creteFile{
+    constructor(nameFile){
+        this.nameFile = nameFile;   
+    };
+    
+    readFile(){
+        const ruta = `./${this.nameFile}.txt`
+
+        try {
+            const datas = fs.readFileSync(ruta,'utf-8');
+            const dataJson = JSON.parse(datas);
+
+
+             return dataJson
+ 
+        } catch (error){
+            if(error.message == "Unexpected end of JSON input"){
+                let mensaje = "No hay contenido"
+                return mensaje
+            }
+               
+            console.log(`Hubo un error: ${error.message}`);
+            return
+        }
+
+    }
+
+    writeFile(producto){
+        const ruta = `./${this.nameFile}.txt`
+
+        try {
+
+        
+            fs.writeFileSync(ruta,JSON.stringify(producto, null, '\t'));
+       
+ 
+        } catch (error){
+            console.log(`Hubo un error: ${error}`);
+        }
+
+    }
+    
+
+}
+
+let productos = new creteFile('Productos');
+let historialMensajes = new creteFile('historialMensajes');
+
 
 
 httpServer.listen(8080, function() {
     console.log('Servidor corriendo en http://localhost:8080');
 })
 
-const messages = [
-    { author: "Juan", text: "¡Hola! ¿Que tal?" },
-    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
-    { author: "Ana", text: "¡Genial!" }
- ];
 
 
- io.on('connection',socket => {
-    console.log('Un cliente se ha conectado');
-    socket.emit('messages', messages);
+let todosLosProductos = productos.readFile();
+let mensajes = historialMensajes.readFile();
 
-    socket.on('new-message', (data) => {
-        messages.push(data);
-        io.sockets.emit('messages', messages);
+// Conexión con Socket.io 
+io.on('connection',socket => {
+    socket.emit('productos',todosLosProductos);
+    socket.emit('mensajes', mensajes);
+
+    socket.on('new-producto', (data) => {
+
+        let todosLosProductos = productos.readFile();
+
+        if(data.Title == "" || data.Price == "" || data.Thumbnail == ""){
+        alert("Completa todos los campos")
+        return
+        }
+
+        data.id = todosLosProductos.length + 1
+    
+        todosLosProductos.push(data)
+    
+        productos.writeFile(todosLosProductos)
+
+        todosLosProductos = productos.readFile();
+
+
+        io.sockets.emit('productos', todosLosProductos);
     });
- });
+
+    socket.on('new-mensaje',(nuevoMensaje) => {
+
+        let mensajes = historialMensajes.readFile();
+        
+                
+                if(mensajes == "No hay contenido"){nuevoMensaje.id = 1
+                } 
+                else { nuevoMensaje.id = mensajes.length + 1 }
+               
+                
+                // Obtener fecha y hora del msg
+                let dataTime = new Date();
+                let hoy = new Date(dataTime)
+            
+                let fecha = hoy.toLocaleDateString()
+                let hora = hoy.toLocaleTimeString()
+        
+                let tiempoMensaje =  `${fecha} ${hora} `
+        
+                nuevoMensaje.dataTimeMensaje = tiempoMensaje
+        
+                if(mensajes == "No hay contenido"){
+        
+                    nuevoMensaje = [nuevoMensaje]
+                    
+                    historialMensajes.writeFile(nuevoMensaje)
+        
+                } else{
+                    mensajes.push(nuevoMensaje)
+            
+                    historialMensajes.writeFile(mensajes)
+                }
+             
+              
+        
+                mensajes = historialMensajes.readFile();
+        
+                
+        
+                io.sockets.emit('mensajes', mensajes);
+
+    });
+
+
+    socket.on('delete-product', (id) =>{
+        let todosLosProductos = productos.readFile();
+
+        todosLosProductos = todosLosProductos.filter((item) => item.id !== id);
+
+        productos.writeFile(todosLosProductos)
+        
+        io.sockets.emit('productos', todosLosProductos);
+
+    })
+
+
+});
+
+
 
 
 
 app.use(express.static("public"));
+
+
+
+app.get('/',(req, res) => {
+
+    console.log("Pagina regargada")
+
+    let todosLosProductos = productos.readFile();
+    let mensajes = historialMensajes.readFile();
+
+    io.on('connection',socket => {
+        socket.emit('productos',todosLosProductos);
+        socket.emit('mensajes', mensajes);
+    
+    })
+   
+       res.redirect('/')
+   
+   
+   })
+
+
+
